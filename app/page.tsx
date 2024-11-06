b5cd23a0512d1399372f9e40e26c56cef11578ea
Usama Bin Gulshan <163780974+usamagulshan@users.noreply.github.com>
2024-11-07 00:20:11 +0500

    Uploaded just now

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,102 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [todo, setTodo] = useState([
+    { task: "Doing HomeWork", id: 1 },
+    { task: "Reading Books", id: 2 },
+  ]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [inputVal, setInput] = useState("");
+  const [id, setId] = useState(0);
+
+  function addItem() {
+    let obj: any = todo.find((item) => item.id == id);
+    if (obj) {
+      let newArr = todo.filter((item) => item.id != obj.id);
+      setTodo([...newArr, { task: inputVal, id: id }]);
+      setInput("");
+      setId(0);
+      return;
+    }
+
+    setTodo([...todo, { task: inputVal, id: id }]);
+    setInput("");
+    setId(0);
+  }
+
+  function editItem(id: any) {
+    let obj: any = todo.find((item) => item.id == id);
+    setInput(obj.task);
+    setId(obj.id);
+  }
+
+  function DelItem (id:any){
+    let newArr = todo.filter((item) => item.id != id);
+      setTodo([...newArr]);
+  }
+
+  return (
+    <div className="main">
+      <h1>Easy Life To-Do App</h1>
+
+      {/* Input Field */}
+
+      <div className="input-here">
+        <input
+          className="task-name"
+          onChange={function (event) {
+            setInput(event.target.value);
+          }}
+          value={inputVal}
+          type="text"
+          placeholder="Enter Your Task Here"
+        />
+
+        <input
+          className="task-id"
+          onChange={function (event: any) {
+            setId(event.target.value);
+          }}
+          value={id}
+          type="number"
+          placeholder="Write Id"
+        />
+
+        <button onClick={addItem} className="add-item">
+          Add Task
+        </button>
+      </div>
+
+      <h1 className="mt-5">Task Lists</h1>
+
+      {/* Grid Columns */}
+
+      <div className="grid">
+        {todo.map(function (item, index) {
+          return (
+            <div className="grid-data" key={index}>
+              <div className="col1">
+                <span className="x">{index + 1}</span>
+                <span onClick={function () {DelItem(item.id)}} className="x cursor-pointer">X</span>
+              </div>
+
+              <div className="col-data">
+                <div>{item.task}</div>
+                <h2
+                  onClick={() => {
+                    editItem(item.id);
+                  }}
+                  className="text-right text-lg cursor-pointer ml-auto w-10"
+                >
+                  Edit
+                </h2>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
