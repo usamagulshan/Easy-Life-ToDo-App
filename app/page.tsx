e6a2d4737cd7b6cb29be5070f172948be1e308cb
Usama Bin Gulshan <163780974+usamagulshan@users.noreply.github.com>
2024-11-07 01:58:52 +0500

    updated just now

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,71 @@
 "use client";
 
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
+
+// Define the type for a Todo item
+interface TodoItem {
+  task: string;
+  id: number;
+}
 
 export default function Home() {
-  const [todo, setTodo] = useState([
+  const [todo, setTodo] = useState<TodoItem[]>([
     { task: "Doing HomeWork", id: 1 },
     { task: "Reading Books", id: 2 },
   ]);
 
-  const [inputVal, setInput] = useState("");
-  const [id, setId] = useState(0);
+  const [inputVal, setInput] = useState<string>("");
+  const [id, setId] = useState<number>(0);
 
   function addItem() {
-    const obj: any = todo.find((item) => item.id == id);
+    // Find the item by id
+    const obj = todo.find((item) => item.id === id);
+
     if (obj) {
-      const newArr = todo.filter((item) => item.id != obj.id);
-      setTodo([...newArr, { task: inputVal, id: id }]);
-      setInput("");
-      setId(0);
-      return;
+      // If the item exists, replace it
+      const newArr = todo.filter((item) => item.id !== obj.id);
+      setTodo([...newArr, { task: inputVal, id }]);
+    } else {
+      // If the item doesn't exist, add a new one
+      setTodo([...todo, { task: inputVal, id }]);
     }
 
-    setTodo([...todo, { task: inputVal, id: id }]);
+    // Clear the input and id
     setInput("");
     setId(0);
   }
 
-  function editItem(id: any) {
-    const obj: any = todo.find((item) => item.id == id);
-    setInput(obj.task);
-    setId(obj.id);
+  function editItem(id: number) {
+    const obj = todo.find((item) => item.id === id);
+    if (obj) {
+      setInput(obj.task);
+      setId(obj.id);
+    }
   }
 
-  function DelItem(id: any) {
-    const newArr = todo.filter((item) => item.id != id);
+  function DelItem(id: number) {
+    const newArr = todo.filter((item) => item.id !== id);
     setTodo([...newArr]);
   }
+
+  // Handle changes for the input fields
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  const handleIdChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setId(Number(event.target.value));
+  };
 
   return (
     <div className="main">
       <h1>Easy Life To-Do App</h1>
 
       {/* Input Field */}
-
       <div className="input-here">
         <input
           className="task-name"
-          onChange={function (event) {
-            setInput(event.target.value);
-          }}
+          onChange={handleInputChange}
           value={inputVal}
           type="text"
           placeholder="Enter Your Task Here"
@@ -56,9 +73,7 @@ export default function Home() {
 
         <input
           className="task-id"
-          onChange={function (event: any) {
-            setId(event.target.value);
-          }}
+          onChange={handleIdChange}
           value={id}
           type="number"
           placeholder="Write Id"
@@ -72,37 +87,30 @@ export default function Home() {
       <h1 className="mt-5">Task Lists</h1>
 
       {/* Grid Columns */}
-
       <div className="grid">
-        {todo.map(function (item, index) {
-          return (
-            <div className="grid-data" key={index}>
-              <div className="col1">
-                <span className="x">{index + 1}</span>
-                <span
-                  onClick={function () {
-                    DelItem(item.id);
-                  }}
-                  className="x cursor-pointer"
-                >
-                  X
-                </span>
-              </div>
-
-              <div className="col-data">
-                <div>{item.task}</div>
-                <h2
-                  onClick={() => {
-                    editItem(item.id);
-                  }}
-                  className="text-right text-lg cursor-pointer ml-auto w-10"
-                >
-                  Edit
-                </h2>
-              </div>
+        {todo.map((item, index) => (
+          <div className="grid-data" key={index}>
+            <div className="col1">
+              <span className="x">{index + 1}</span>
+              <span
+                onClick={() => DelItem(item.id)}
+                className="x cursor-pointer"
+              >
+                X
+              </span>
             </div>
-          );
-        })}
+
+            <div className="col-data">
+              <div>{item.task}</div>
+              <h2
+                onClick={() => editItem(item.id)}
+                className="text-right text-lg cursor-pointer ml-auto w-10"
+              >
+                Edit
+              </h2>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
